e02959efcbbfe5730e5d51cdd84448cdac361222
Doz <ben.pakpannn@gmail.com>
2025-01-17 00:04:55 -0500

    chore: update setup script

--- pb-setup.sh
+++ pb-setup.sh
@@ -1,7 +1,21 @@
-if [ ! -d "pb" ]; then \
-    git clone --no-checkout git@github.com:doz-8108/protobufs.git pb && \
-    cd pb && \
-    git sparse-checkout init && \
-    git sparse-checkout set visitor-counter-svc && \
-    git checkout main && mv ./visitor-counter-svc/* . && rm -rf ./visitor-counter-svc; \
+git rm --cached pb> /dev/null 2>&1
+
+if [ ! -f ".gitmodules" ]; then
+    touch .gitmodules
+fi
+
+dependencies=("visitor-counter-svc")
+if [ ! -d "pb" ]; then
+    git submodule add --force "git@github.com:doz-8108/protobufs.git" pb
+    cd pb
+
+    folders=$(find . -maxdepth 1 -type d ! -name '.' ! -name '..' | sed 's|^\./||')
+    for folder in ${folders[@]}; do
+        if [[ ! " ${dependencies[*]} " =~ $folder ]]; then
+            rm -rf $folder 
+        else
+            mv ./$folder/* ./
+            rm -rf $folder
+        fi
+    done
 fi
